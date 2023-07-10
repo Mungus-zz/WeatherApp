5114b738c26b029c4753ab751e95139f4b004858
Mungus-zz <erwinn.villanueva@gmail.com>
2023-07-10 12:43:09 -0700

    Updated LocalWeather screen
    Adjusted dataCard to reflect probability of rain for rest of the day. Adjusted styling of dataCard.

--- src/screens/LocalWeather.tsx
+++ src/screens/LocalWeather.tsx
@@ -7,6 +7,45 @@ import { fetchWeatherData } from '../../services/OpenWeatherMapAPI';
 import { weatherConditions, WeatherConditionCode } from '../animations/weatherAnimation';
 import { ScrollView } from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  searchbar: {
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  button: {
+    marginBottom: 20,
+  },
+  dataCard: {
+    flex: 1,
+    marginBottom: 20,
+    padding: 20,
+  },
+  animationCard: {
+    flex: 3,
+  },
+  lottie: {
+    width: '100%',
+    height: '100%',
+    alignSelf: 'center',
+  },
+  paragraph: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: '#333',
+    padding: 5,
+  },
+  title: {
+    fontSize: 22,
+    marginBottom: 20,
+    color: '#3F51B5',
+    fontWeight: 'bold',
+  },
+});
+
 
 export const LocalWeather = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -24,101 +63,74 @@ export const LocalWeather = () => {
     }
   };
 
-  let isDay, timeOfDay, weatherCondition, animation, isRaining;
+  let isDay, timeOfDay, weatherCondition, animation, isRaining, maxChanceOfRain = 0;
 
-  if (weatherData) {
-    isRaining = weatherData.current.weather.some((condition: { main: string }) => condition.main.toLowerCase().includes('rain'));
+if (weatherData) {
+  isRaining = weatherData.current.weather.some((condition: { main: string }) => condition.main.toLowerCase().includes('rain'));
 
-    const currentTime = Math.floor(new Date().getTime() / 1000);
-    isDay = currentTime > weatherData.current.sunrise && currentTime < weatherData.current.sunset;
-    timeOfDay = isDay ? 'day' : 'night';
+  const currentTime = Math.floor(new Date().getTime() / 1000);
+  isDay = currentTime > weatherData.current.sunrise && currentTime < weatherData.current.sunset;
+  timeOfDay = isDay ? 'day' : 'night';
 
-    weatherCondition = weatherData.current.weather[0].description.toLowerCase();
+  weatherCondition = weatherData.current.weather[0].description.toLowerCase();
 
-    animation = weatherConditions[timeOfDay as 'day' | 'night'][weatherCondition as WeatherConditionCode];
-  }
+  animation = weatherConditions[timeOfDay as 'day' | 'night'][weatherCondition as WeatherConditionCode];
 
-  return (
-    <View style={styles.container}>
-      <Searchbar
-        placeholder="Search for a location"
-        onChangeText={onChangeSearch}
-        value={searchQuery}
-        style={styles.searchbar}
-      />
-      <Button mode="contained" onPress={fetchWeather} style={styles.button}>
-        Get Weather
-      </Button>
-      {weatherData && (
-  <Card style={styles.dataCard}>
-    <ScrollView>
-      <Card.Content>
-        <Title>Weather in {searchQuery}</Title>
-        <Paragraph>{((weatherData.current.temp - 273.15) * 9/5 + 32).toFixed(2)}°F</Paragraph>
-        <Paragraph style={styles.paragraph}>
-          Weather: {weatherData.current.weather[0].main}
-        </Paragraph>
-        <Paragraph style={styles.paragraph}>
-          Description: {weatherData.current.weather[0].description}
-        </Paragraph>
-        <Paragraph style={styles.paragraph}>
-          Rain: {isRaining ? 'Yes' : 'No'}
-        </Paragraph>
-        <Paragraph style={styles.paragraph}>
-          Chance of Rain: {weatherData.hourly[0].pop * 100}%
-        </Paragraph>
-      </Card.Content>
-    </ScrollView>
-  </Card>
-)}
-      {animation && (
-        <Card style={styles.animationCard}>
-          <Card.Content>
-            <LottieView source={animation} autoPlay loop style={styles.lottie} />
-          </Card.Content>
-        </Card>
-      )}
-      <Snackbar
-        visible={error !== null}
-        onDismiss={() => setError(null)}
-        action={{
-          label: 'Dismiss',
-          onPress: () => setError(null),
-        }}
-      >
-        {error}
-      </Snackbar>
-    </View>
-  );
-};
+  // Calculate the maximum chance of rain for the rest of the day
+  const restOfDay = weatherData.hourly.filter((hour: { dt: number }) => hour.dt > currentTime);
+  maxChanceOfRain = Math.max(...restOfDay.map((hour: { pop: number }) => hour.pop));
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-  searchbar: {
-    marginTop: 30,
-    marginBottom: 20,
-  },
-  button: {
-    marginBottom: 20,
-  },
-  dataCard: {
-    flex: 1,
-    marginBottom: 20,
-  },
-  animationCard: {
-    flex: 3,
-  },
-  lottie: {
-    width: '100%',
-    height: '100%',
-    alignSelf: 'center',
-  },
-  paragraph: {
-    fontSize: 20,
-    marginBottom: 20,
-    color: '#333',
-  },
-});
+return (
+  <View style={styles.container}>
+    <Searchbar
+      placeholder="Search for a location"
+      onChangeText={onChangeSearch}
+      value={searchQuery}
+      style={styles.searchbar}
+    />
+    <Button mode="contained" onPress={fetchWeather} style={styles.button}>
+      Get Weather
+    </Button>
+    {weatherData && (
+      <Card style={styles.dataCard}>
+        <ScrollView>
+        <Card.Content>
+  <Title style={styles.title}>Weather in {searchQuery}</Title>
+  <Paragraph>{((weatherData.current.temp - 273.15) * 9/5 + 32).toFixed(2)}°F</Paragraph>
+  <Paragraph style={styles.paragraph}>
+    Weather: {weatherData.current.weather[0].main}
+  </Paragraph>
+  <Paragraph style={styles.paragraph}>
+    Description: {weatherData.current.weather[0].description}
+  </Paragraph>
+  <Paragraph style={styles.paragraph}>
+    Rain: {isRaining ? 'Yes' : 'No'}
+  </Paragraph>
+  <Paragraph style={styles.paragraph}>
+    Maximum Chance of Rain for the Rest of the Day: {maxChanceOfRain * 100}%
+  </Paragraph>
+</Card.Content>
+        </ScrollView>
+      </Card>
+    )}
+    {animation && (
+      <Card style={styles.animationCard}>
+        <Card.Content>
+          <LottieView source={animation} autoPlay loop style={styles.lottie} />
+        </Card.Content>
+      </Card>
+    )}
+    <Snackbar
+      visible={error !== null}
+      onDismiss={() => setError(null)}
+      action={{
+        label: 'Dismiss',
+        onPress: () => setError(null),
+      }}
+    >
+      {error}
+    </Snackbar>
+  </View>
+);
+    }
